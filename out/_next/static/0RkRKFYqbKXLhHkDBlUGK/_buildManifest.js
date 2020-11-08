286c43b09cdf0974dce24195f87e09a512bb3b4b
Paritosh Batish <batishparitosh2@gmail.com>
2020-11-08 17:30:07 +0530

    removed out

--- out/_next/static/0RkRKFYqbKXLhHkDBlUGK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F35a27a54f3ada5bf18cd.css","static\u002Fchunks\u002Fpages\u002Findex-0591b2ac73832a469f54.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
